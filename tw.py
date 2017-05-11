9bfdc3fc73b3f5ee568242e859c0178953e2935d
Pawel M <obramanix@gmail.com>
2017-05-11 13:03:28 +0200

    There is always place for improvement
    fixed travel time conditionals.

--- tw.py
+++ tw.py
@@ -37,6 +37,54 @@ def load_villages():
 			villages.append(ptr)
 	return villages
 
+def farm(browser, element_name, warriors, village, max_h, max_min):
+	#light
+	elem = browser.find_element_by_name(element_name)
+	elem.send_keys(warriors)
+	
+	#village
+	elem = browser.find_element_by_name('input')
+	elem.send_keys(village)
+	time.sleep(1)
+	
+	#confirm
+	browser.find_element_by_id("target_attack").click()
+	time.sleep(1)
+	im_robot(browser)
+	
+	#travel time conditional
+	try:
+		timer = browser.find_element_by_xpath('//*[@id="command-data-form"]/table[1]/tbody/tr[3]/td[2]').text
+	except:
+		browser.refresh()
+		time.sleep(1)
+		timer = browser.find_element_by_xpath('//*[@id="command-data-form"]/table[1]/tbody/tr[3]/td[2]').text
+	if (int(timer.split(":")[0]) * 60 + int(timer.split(":")[1])) > (max_h * 60 + max_min):
+		elem = browser.find_element_by_xpath("/html/body")
+		elem.send_keys("5")
+		wait_for_page(browser, 5, "Symulator")
+		print("\nAttack on: ", village)
+		print("\t time too long!: ", timer)
+	else:
+		browser.find_element_by_id("troop_confirm_go").click()
+		print("\nAttack on: ", village)
+		print("\t+send: ", warriors, " ", element_name)
+		print("\t time: ", timer)
+		wait_for_page(browser, 15, "Symulator")
+
+def godela(browser):
+	try:
+		browser.find_element_by_id("event_shop_button")
+	except:
+		browser.find_element_by_class_name('menu-event-icon').click()
+	while(1):
+		try:
+			browser.find_element_by_link_text('Wyzwij').click()
+			print("A ty ile masz godeu?")
+			time.sleep(5)
+		except:
+			 break;
+
 f = open("../tribal_wars",'r')
 l = f.readline()
 p = f.readline()
@@ -102,69 +150,16 @@ while(1):
 			print("\tlight: ", light_num)
 			print("\taxes: ", axe_num)
 		
-		
 		if light_num >= lk:
-			#light
-			elem = browser.find_element_by_name("light")
-			elem.send_keys(lk)
-			
-			#village
-			elem = browser.find_element_by_name('input')
-			elem.send_keys(village)
-			time.sleep(1)
-			
-			#confirm
-			browser.find_element_by_id("target_attack").click()
-			time.sleep(1)
-			im_robot(browser)
-			
-			timer = browser.find_element_by_xpath('//*[@id="command-data-form"]/table[1]/tbody/tr[3]/td[2]').text
-			if int(timer.split(":")[0]) > max_h and int(timer.split(":")[1]) > max_min:
-				index += 1
-				elem = browser.find_element_by_xpath("/html/body")
-				elem.send_keys("5")
-				wait_for_page(browser, 5, "Symulator")
-				print("\nAttack on: ", village)
-				print("\t time too long!: ", timer)
-			else:
-				browser.find_element_by_id("troop_confirm_go").click()
-				
-				print("\nAttack on: ", village)
-				print("\t+send: ", lk, " lk")
-				print("\t time: ", timer)
-				wait_for_page(browser, 15, "Symulator")
-				index += 1
+			#lk
+			farm(browser, "light", lk, village, max_h, max_min)
+			index += 1
 		elif  axe_num >= axes:
 			#axes
-			elem = browser.find_element_by_name("axe")
-			elem.send_keys(axes)
-			
-			elem = browser.find_element_by_name('input')
-			elem.send_keys(village)
-			time.sleep(1)
-			
-			#confirm
-			browser.find_element_by_id("target_attack").click()
-			time.sleep(1)
-			im_robot(browser)
-			
-			timer = browser.find_element_by_xpath('//*[@id="command-data-form"]/table[1]/tbody/tr[3]/td[2]').text
-			if int(timer.split(":")[0]) > max_h and int(timer.split(":")[1]) > max_min:
-				index += 1
-				elem = browser.find_element_by_xpath("/html/body")
-				elem.send_keys("5")
-				wait_for_page(browser, 5, "Symulator")
-				print("\nAttack on: ", village)
-				print("\t time too long!: ", timer)
-			else:
-				browser.find_element_by_id("troop_confirm_go").click()
-				
-				print("\nAttack on: ", village)
-				print("\t+send: ", axes, " axes")
-				print("\t time: ", timer)
-				wait_for_page(browser, 15, "Symulator")
-				index += 1
+			farm(browser, "axe", axes, village, max_h, max_min)
+			index += 1
 		else:
+			#no warriors :(
 			elem = browser.find_element_by_xpath("/html/body")
 			elem.send_keys("d")
 			wait_for_page(browser, 5, "Symulator")
@@ -172,22 +167,13 @@ while(1):
 				village_count -= 1
 			else:
 				print("i ll wait...")
-				min_time = random.randint(16,624)
+				min_time = random.randint(160,624)
 				print(min_time, "sec")
 				village_count = num_villages
 				timeout = min_time
 	#~ else:
-		#~ try:
-			#~ browser.find_element_by_id("event_shop_button")
-		#~ except:
-			#~ browser.find_element_by_class_name('menu-event-icon').click()
-		#~ while(1):
-			#~ try:
-				#~ browser.find_element_by_link_text('Wyzwij').click()
-				#~ print("A ty ile masz godeu?")
-				#~ time.sleep(5)
-			#~ except:
-				#~ break;
+	#ile masz godel function
+		#godela(browser)
 	if index == len(villages):
 		index = 0
 		timeout = 10
