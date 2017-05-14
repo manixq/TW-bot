b866f5878220469983243c873b818654dea698f4
Pawel M <obramanix@gmail.com>
2017-05-14 15:23:30 +0200

    Added PyQt4 GUI

--- tw.py
+++ tw.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import base64
 from selenium import webdriver
@@ -9,9 +10,10 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import random
 
-max_h = 2
-max_min = 30
-num_villages = 2
+#gui
+from PyQt4 import QtCore, QtGui
+from gui import Ui_Dialog
+from PyQt4.QtCore import pyqtSlot,SIGNAL,SLOT
 
 def im_robot(browser):
 	if (len(browser.find_elements_by_id("bot_check_image"))>0) :
@@ -33,8 +35,7 @@ def load_villages():
 	villages = []
 	with open("../target.txt") as data: 
 		for line in data:
-			ptr = line.split()
-			villages.append(ptr)
+			villages.append(line)
 	return villages
 
 def farm(browser, element_name, warriors, village, max_h, max_min):
@@ -85,104 +86,200 @@ def godela(browser):
 		except:
 			 break;
 
-f = open("../tribal_wars",'r')
-l = f.readline()
-p = f.readline()
-f.close()
+def log_in(browser, world):
+	f = open("../tribal_wars",'r')
+	l = f.readline()
+	p = f.readline()
+	f.close()
+	
+	l = str(base64.decodebytes(l.encode('utf-8')).decode("utf-8"))
+	p = str(base64.decodebytes(p.encode('utf-8')).decode("utf-8"))
+	
+	if browser == 0:
+		browser = webdriver.Chrome(executable_path=r"..\chromedriver.exe")
+	else:
+		browser = webdriver.Firefox(executable_path=r"..\geckodriver.exe")
 
-l = str(base64.decodebytes(l.encode('utf-8')).decode("utf-8"))
-p = str(base64.decodebytes(p.encode('utf-8')).decode("utf-8"))
-
-#bot starts work
-#browser = webdriver.Firefox(executable_path=r"..\geckodriver.exe")
-browser = webdriver.Chrome(executable_path=r"..\chromedriver.exe")
-
-browser.get('https://www.plemiona.pl/')
-time.sleep(1)
-
-elem = browser.find_element_by_name('username')  
-elem.send_keys(l)
-elem = browser.find_element_by_name('password') 
-elem.send_keys(p + Keys.RETURN)
-
-#village
-wait_for_page(browser, 5, "Świat 115")
-browser.get("https://www.plemiona.pl/page/play/pl115")
-villages = load_villages()
-
-#place
-elem = browser.find_element_by_xpath("/html/body")
-elem.send_keys("5")
-wait_for_page(browser, 5, "Symulator")
-
-
-index = 0
-light_num = 0
-axe_num = 0
-timeout = 0
-village_count = num_villages
-while(1):
+	browser.get('https://www.plemiona.pl/')
 	time.sleep(1)
-	if timeout <= 0:
-		village = villages[index][0]
-		axes = int(villages[index][1])
-		lk = int(villages[index][2])
-		
-		try:
-			elem = browser.find_element_by_id('units_entry_all_axe')
-		except NoSuchElementException:
-			#place
-			elem = browser.find_element_by_xpath("/html/body")
-			elem.send_keys("5")
-			wait_for_page(browser, 5, "Symulator")
-		
-		pre_axe = axe_num
-		pre_light = light_num
-		
-		elem = browser.find_element_by_id('units_entry_all_axe')
-		axe_num = int(elem.text[1:-1])
-		
-		elem = browser.find_element_by_id('units_entry_all_light')
-		light_num = int(elem.text[1:-1])
-		
-		if pre_axe != axe_num or pre_light != light_num:
-			print("\nAvailable units:")
-			print("\tlight: ", light_num)
-			print("\taxes: ", axe_num)
-		
-		if light_num >= lk:
-			#lk
-			farm(browser, "light", lk, village, max_h, max_min)
-			index += 1
-		elif  axe_num >= axes:
-			#axes
-			farm(browser, "axe", axes, village, max_h, max_min)
-			index += 1
-		else:
-			#no warriors :(
-			elem = browser.find_element_by_xpath("/html/body")
-			elem.send_keys("d")
-			wait_for_page(browser, 5, "Symulator")
-			if(village_count > 0):
-				village_count -= 1
-			else:
-				print("i ll wait...")
-				min_time = random.randint(160,624)
-				print(min_time, "sec")
-				village_count = num_villages
-				timeout = min_time
-	#~ else:
-	#ile masz godel function
-		#godela(browser)
-	if index == len(villages):
-		index = 0
-		timeout = 10
-		print("\n====RESTART====\n")
-	timeout = timeout - 1
 
-wait_for_page(browser, 5, "Wyloguj")
-browser.find_element_by_partial_link_text("Wyloguj").click()
-browser.close()
+	elem = browser.find_element_by_name('username')  
+	elem.send_keys(l)
+	elem = browser.find_element_by_name('password') 
+	elem.send_keys(p + Keys.RETURN)
+
+	#village
+	wait_for_page(browser, 5, "Świat 115")
+	browser.get("https://www.plemiona.pl/page/play/" + world)
+	return browser
+
+@pyqtSlot()
+def go_farm(browser_, world_, army_, villages_number_, wait_time_, max_range_):
+	#bot starts work
+	browser = log_in(browser_, world_)
+	villages = load_villages()
+
+	#place
+	elem = browser.find_element_by_xpath("/html/body")
+	elem.send_keys("5")
+	wait_for_page(browser, 5, "Symulator")
+
+	index = 0
+	
+	spear_num = 0
+	sword_num = 0
+	axe_num = 0
+	light_num = 0
+	scout_num = 0
+	ram_num = 0
+	cata_num = 0
+	snob_num = 0
+	
+	wait_times = wait_time_.split(":")
+	max_h = int(max_range_.split(":")[0])
+	max_min = int(max_range_.split(":")[1])
+	
+	timeout = 0
+	village_count = villages_number_
+	while(1):
+		time.sleep(1)
+		if timeout <= 0:
+			village = villages[index]
+			
+			spear = army_[0]
+			sword = army_[1]
+			axe = army_[2]
+			light = army_[3]
+			scout = army_[4]
+			ram = army_[5]
+			cata = army_[6]
+			snob = army_[7]
+			
+			try:
+				elem = browser.find_element_by_id('units_entry_all_axe')
+			except NoSuchElementException:
+				#place
+				elem = browser.find_element_by_xpath("/html/body")
+				elem.send_keys("5")
+				wait_for_page(browser, 5, "Symulator")
+			
+			pre_spear = spear_num
+			pre_sword = sword_num
+			pre_axe = axe_num
+			pre_light = light_num
+			pre_scout = scout_num
+			pre_ram = ram_num
+			pre_cata = cata_num
+			pre_snob = snob_num
+			
+			elem = browser.find_element_by_id('units_entry_all_spear')
+			spear_num = int(elem.text[1:-1])
+			elem = browser.find_element_by_id('units_entry_all_sword')
+			sword_num = int(elem.text[1:-1])			
+			elem = browser.find_element_by_id('units_entry_all_axe')
+			axe_num = int(elem.text[1:-1])
+			elem = browser.find_element_by_id('units_entry_all_light')
+			light_num = int(elem.text[1:-1])			
+			elem = browser.find_element_by_id('units_entry_all_spy')
+			scout_num = int(elem.text[1:-1])			
+			elem = browser.find_element_by_id('units_entry_all_ram')
+			ram_num = int(elem.text[1:-1])			
+			elem = browser.find_element_by_id('units_entry_all_catapult')
+			cata_num = int(elem.text[1:-1])
+			elem = browser.find_element_by_id('units_entry_all_snob')
+			snob_num = int(elem.text[1:-1])
+			
+			if (pre_spear != spear_num or pre_sword != sword_num or 
+				pre_axe != axe_num or pre_light != light_num or 
+				pre_scout != scout_num or pre_ram != ram_num or 
+				pre_cata != cata_num or pre_snob != snob_num):
+				
+				print("\nAvailable units:")
+				print("\tspear: ", spear_num)
+				print("\tsword: ", sword_num)
+				print("\taxes: ", axe_num)
+				print("\tlight: ", light_num)
+				print("\tscout: ", scout_num)
+				print("\tram: ", ram_num)
+				print("\tcata: ", cata_num)
+				print("\tsnob: ", snob_num)
+			
+			if light_num >= light and light != 0:
+				#lk
+				farm(browser, "light", light, village, max_h, max_min)
+				index += 1
+			elif  axe_num >= axe and axe != 0:
+				#axes
+				farm(browser, "axe", axe, village, max_h, max_min)
+				index += 1
+			elif  sword_num >= sword and sword != 0:
+				#axes
+				farm(browser, "sword", sword, village, max_h, max_min)
+				index += 1
+			elif  spear_num >= spear and spear != 0:
+				#axes
+				farm(browser, "spear", spear, village, max_h, max_min)
+				index += 1
+			else:
+				#no warriors :(
+				elem = browser.find_element_by_xpath("/html/body")
+				elem.send_keys("d")
+				wait_for_page(browser, 5, "Symulator")
+				if(village_count > 0):
+					village_count -= 1
+				else:
+					print("i ll wait...")
+					min_time = random.randint(int(wait_times[0]),int(wait_times[1]))
+					print(min_time, "sec")
+					village_count = villages_number_
+					timeout = min_time
+		#~ else:
+		#ile masz godel function
+			#godela(browser)
+		if index == len(villages):
+			index = 0
+			timeout = 10
+			print("\n====RESTART====\n")
+		timeout = timeout - 1
+
+class MyDialog(QtGui.QDialog):
+	def __init__(self, parent=None):
+		QtGui.QWidget.__init__(self, parent)
+		self.ui = Ui_Dialog()
+		self.ui.setupUi(self)
+	
+	def go_farm(self):
+		browser_ = self.ui.viewer.currentIndex()
+		world_ = self.ui.world_edit.text()
+		army_ = [self.ui.spear_box.value(),
+			self.ui.sword_box.value(),
+			self.ui.axe_box.value(),
+			self.ui.light_box.value(),
+			self.ui.scout_box.value(),
+			self.ui.ram_box.value(),
+			self.ui.cata_box.value(),
+			self.ui.snob_box.value()]
+		villages_number_ = self.ui.villages_box.value()
+		wait_time_ = self.ui.wait_time_edit.text()
+		max_range_ = self.ui.max_range_edit.text()
+		self.hide()
+		try:
+			go_farm(browser_, world_, army_, villages_number_, wait_time_, max_range_)
+		except BaseException as error:
+			self.ui.log_edit.appendPlainText(error)
+		self.show()
+
+if __name__ == "__main__":
+	app = QtGui.QApplication(sys.argv)
+	myapp = MyDialog()
+	
+	myapp.ui.go_farm_button.clicked.connect(myapp.go_farm)
+	myapp.show()
+	sys.exit(app.exec_())
+
+#~ wait_for_page(browser, 5, "Wyloguj")
+#~ browser.find_element_by_partial_link_text("Wyloguj").click()
+#~ browser.close()
 
 
 
